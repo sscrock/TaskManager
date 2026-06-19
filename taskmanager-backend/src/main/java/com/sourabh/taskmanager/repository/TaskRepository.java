c45d823d60a44bf36b000aef216b1cc23f612d1a
sscrock <sscrock1234@gmail.com>
2026-06-19 17:31:07 +0530

    Add completed task query method

--- taskmanager-backend/src/main/java/com/sourabh/taskmanager/repository/TaskRepository.java
+++ taskmanager-backend/src/main/java/com/sourabh/taskmanager/repository/TaskRepository.java
@@ -3,5 +3,8 @@ package com.sourabh.taskmanager.repository;
 import com.sourabh.taskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task,Long> {
+    List<Task> findByCompleted(Boolean completed);
 }
